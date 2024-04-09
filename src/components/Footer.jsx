bd8a9b241de108308822b917b9b58cb274d011e4
julianespa <julianespa12@gmail.com>
2024-04-09 17:34:12 +1000

    finish footer content and styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,53 @@
-function Footer () {
+import { NavLink } from "react-router-dom"
+
+function Footer() {
     let year = new Date().getFullYear()
     return (
-        <div className="footer">
-            <p>Copyright © {year}</p>
-        </div>
+        <section className="footer">
+            <div className="footer-content">
+                <div id="links">
+                    <h5>Magia Bakery</h5>
+                    <NavLink to={`/`}>Home</NavLink>
+                    <NavLink to={`/about`}>About</NavLink>
+                    <NavLink to={`/contact`}>Contact</NavLink>
+                    <NavLink to={`/products`}>Products</NavLink>
+                </div>
+                <div id="footer-markets">
+                    <h5>Markets</h5>
+                    <div className="footer-markets-info">
+                        <div className="footer-day">
+                            <h6>Saturdays</h6>
+                            <div>
+                                <p className="market-name">West End Markets</p>
+                                <p>Montague Road & Jane Street, West End QLD</p>
+                            </div>
+                            <div>
+                                <p className="market-name">Carseldine Markets</p>
+                                <p>133 Dorville Road, Carseldine, Brisbane, QLD</p>
+                            </div>
+                        </div>
+                        <div className="footer-day">
+                            <h6>Sundays</h6>
+                            <div>
+                                <p className="market-name">Nundah Markets</p>
+                                <p>Station St, Nundah QLD</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="footer-day">
+                    <h5>Pick Up</h5>
+                    <p className="market-name">Magia Bakery</p>
+                    <p>166 Wickham Terrace</p>
+                </div>
+                <div className="d-flex flex-column">
+                    <h5>Social Media</h5>
+                    <a href="https://www.instagram.com/magia.bycoky/" target="_blank" rel='noopener noreferrer'>Instagram</a>
+                    <a href="https://www.facebook.com/magia.bycoky/" target="_blank" rel='noopener noreferrer'>Facebook </a>
+                </div>
+            </div>
+            <p>All rights reserved. © {year}</p>
+        </section>
     )
 }
 
