42165c4b05b543bf368d25e89f78ac73cb3472a9
julianespa <julianespa12@gmail.com>
2024-04-10 12:19:06 +1000

    products animation loading

---
+++ src/components/Products.jsx
@@ -0,0 +1,17 @@
+import { NavLink } from "react-router-dom"
+
+const Products = () => {
+    return (
+        <main className="products">
+            <h1>Our Products Page</h1>
+            <p>This section is not ready yet. Contact us for Product details</p>
+            <button>
+                <NavLink to={"/contact"}>Get in touch</NavLink>
+            </button>
+
+            <div className="img"><img src="logoMagia.jpg" alt="logo magia bakery" /></div>
+        </main>
+    )
+}
+
+export default Products
