2bbcb5eef166f6887f7f92486fd540fe8bb86fb9
julianespa <julianespa12@gmail.com>
2024-04-10 10:36:47 +1000

    create styles about page

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,31 +1,43 @@
-function About () {
+import { NavLink } from "react-router-dom"
+
+function About() {
     return (
         <>
             <div className="aboutPage">
-                <div>
-                    <img width={300} src="/about/pavloba.jpg" alt="" />
+                <main className="bio">
+                    <article>
+                        <img src="logoMagia.jpg" alt="logo magia bakery" />
+                        <p>
+                            Hey there! I´m Vicky & this is my sweet dream come true: MAGIA.
+                        </p>
+                        <p>
+                            A forever sweet tooth & pastry lover, that has been self teaching herself through all things sweet & wants to share them with you.
+                        </p>
+                        <p>
+                            As an Argentinian, I can´t help trying to put dulce de leche on everything, so here you´ll find traditional Argentinian alfajores (or cookie sandwiches if that´s easier to pronounce), mind blowing cookies & a selection of cakes that are all you need for your celebrations & get togethers.
+                        </p>
+                        <p>
+                            Do you have a special occasion? We are here for you! We´d love to make the most beautiful & original piece for you to share, give as a gift for someone you love (yourself included, of course) & create a sweet memory.
+                        </p>
+                    </article>
+                </main>
+
+                <article className="goal">
+                    <img src="about/alfajor-corazon.jpg" alt="heart chocolate alfajor" />
+                    <h2>OUR GOAL</h2>
                     <p>
-                    Hey there! I´m Vicky & this is my sweet dream come true: MAGIA.
+                        We want to offer you the best sweet experience you can have.
                     </p>
-                </div>
-                <div>
-                    <img width={300} src="/about/alfajor-corazon.jpg" alt="" />
                     <p>
-                    A forever sweet tooth & pastry lover, that has been self teaching herself through all things sweet & wants to share them with you.
+                        Using the freshest & highest quality ingredients available, family recipes, personal inventions & creativity to  reach that goal. Do you wanna try?
                     </p>
-                </div>
-                <div>
-                    <img src="/about/mitad-blanco.jpg" width={300} alt="" />
-                    <p>
-                    As an Argentinian, I can´t help trying to put dulce de leche on everything, so here you´ll find traditional Argentinian alfajores (or cookie sandwiches if that´s easier to pronounce), mind blowing cookies & a selection of cakes that are all you need for your celebrations & get togethers.
-                    </p>
-                </div>
-                <div>
-                    <img src="/about/mitad-choc.jpg" width={300} alt="" />
-                    <p>
-                    Do you have a special occasion? We are here for you! We´d love to make the most beautiful & original piece for you to share, give as a gift for someone you love (yourself included, of course) & create a sweet memory.
-                    </p>
-                </div>
+
+                    <button>
+                        <NavLink to={"/products"}>Show me more</NavLink>
+                    </button>
+                </article>
+
+
             </div>
         </>
     )
